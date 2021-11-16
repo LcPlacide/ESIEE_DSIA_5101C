42b884f3f81e3fb170c2b53b4ca4d22657e62fb8
LcPlacide <loicplacide@orange.fr>
2021-11-16 11:59:20 +0100

    motus v2

--- labs/src/main/scala/fr/esiee/fpic/labs/motus.scala
+++ labs/src/main/scala/fr/esiee/fpic/labs/motus.scala
@@ -1,17 +1,19 @@
 package fr.esiee.fpic.labs:
     import scala.util.Random
     import scala.io.StdIn.readLine
+    import scala.compiletime.ops.boolean
 
-    def clear() = print("\u001b[2J")
+    def clear() = print("\u001b[2J\n")
 
     class Word(val str:String=""){
-        val index = for (i<-0 to str.length-1) yield i
+        val length = str.length
+        val index = (0 to length-1).toList //for (i<-0 to str.length-1) yield i
         val letterOrder = for (i<-index) yield (str(i),i) 
         //index.foldLeft(List[(Char,Int)]()) ((xs:List[(Char, Int)], x:Int)=> xs:::List((str(x),x)))
         val letters = for (i<-index) yield (str(i),-1) 
         //index.foldLeft(List[(Char,Int)]()) ((xs:List[(Char, Int)], x:Int)=> xs:::List((str(x),-1)))
-        val mask = (for (i<-index) yield (if i==0 then str(i) else "_")).foldLeft("")(_+_)
-        //index.foldLeft(List[String]()) ((xs:List[Matchable], x:Int)=> if x==0 then xs:::List(str(x)) else xs:::List('_')).foldLeft("")(_+_)
+        val mask = (for (i<-index) yield (if i==0 then str(i) else "*")).foldLeft("")(_+_)
+        //index.foldLeft(List[String]()) ((xs:List[Matchable], x:Int)=> if x==0 then xs:::List(str(x)) else xs:::List('*')).foldLeft("")(_+_)
 
         override def toString: String = str
 
@@ -27,10 +29,11 @@ package fr.esiee.fpic.labs:
     class Dict(fromFile: String="", fromList: List[Word]=List(), enc:String = "UTF-8"){
         val it = if fromFile!="" then scala.io.Source.fromFile(fromFile, enc).getLines.toList else List() 
         val all_words = if it!=List() then (for (w<-it) yield Word(w)).toList else fromList
+        val size = all_words.length
 
         def selectWords(size:Int=0, toInclude:Set[(Char, Int)]=Set(), toExclude:Set[(Char, Int)]=Set()): Dict = 
             val sizeFilter = size match {
-                case n:Int if n>0 => Dict(fromList=(for (w <- all_words; if w.str.length == size) yield w).toList)
+                case n:Int if n>0 => Dict(fromList=(for (w <- all_words; if w.length == size) yield w).toList)
                 case _ => this
             }
             
@@ -63,13 +66,13 @@ package fr.esiee.fpic.labs:
                 case true => (for (w<-all_words ; if w.str==str) yield w).head
                 case false => Word()
 
-        override def toString : String = s"Dict(${all_words.length})"
+        override def toString : String = s"Dict(${size})"
     }
 
-    class Proposal(val answer:Word, val correct:Word){
+    class Proposal(val answer:Word, val correct:Word, hide:Boolean=false){
         val color = this.evalProposal()
-        val coloredAnswer = for (i<-answer.index) yield(s"${color(i)}${answer.str(i)}")
-        //answer.index.foldLeft(List[String]()) ((xs:List[String], x:Int)=> xs:::List(s"${color(x)}${answer.str(x)}"))
+        val coloredAnswer = for (i<-answer.index) yield(if hide then s"${color(i)}${answer.mask(i)}" else s"${color(i)}${answer.str(i)}")
+        //answer.index.foldLeft(List[String]()) ((xs:List[String], x:Int)=> if hide then xs:::List(s"${color(x)}${answer.mask(x)}") else xs:::List(s"${color(x)}${answer.str(x)}"))
         val isCorrect = answer.str == correct.str
         val goodLetters = for (i<-answer.index ; if color(i)!=Console.BLUE) yield(if color(i)==Console.RED then answer.letterOrder(i) else answer.letters(i))
         //answer.index.foldLeft(List[(Char,Int)]()) ((xs:List[(Char,Int)], x:Int)=> if color(x)==Console.RED then xs:::List(answer.letterOrder(x)) else if colox(x)==Console.YELLOW then xs:::List(answer.letters(x)) else xs)
@@ -93,50 +96,52 @@ package fr.esiee.fpic.labs:
             coloredAnswer.toList.foldLeft("")(_+_)+Console.RESET
     }
 
-    def make_proposal(retry:Int, player:Map[String,Dict], dict_machine:Dict, correct:Word): Unit =
-        val answer = retry match {
-            case r: Int if r>=0 => readLine("Votre proposition ? ").toUpperCase()
+    def make_proposal(retry:Int, playerAnswers:Map[String,Dict], machineAnswers:Map[String,Dict], correct:Word, hideMachine:Boolean=true): Unit =
+        val currentPlayerAnswer = retry match {
+            case r: Int if r>=0 => readLine(s"Votre proposition ${(playerAnswers("last").size+1)} sur ${(playerAnswers("last").size+retry)} ? ").toUpperCase()
             case _ => "Vous avez perdu."
         }
 
-        if answer=="Vous avez perdu." then 
+        if currentPlayerAnswer=="Vous avez perdu." then 
             println(s"Tentative maximal atteinte. Le mot est ${correct.str}. DEFAITE...")
             return()
-        else if player("dico").isValidWord(answer)==false then 
-            println(s"${answer} n'est pas dans le dictionnaire du jeu. Entrer autre chose.")
-            make_proposal(retry, player, dict_machine, correct)
-        else if answer.length != correct.str.length then
-            println(s"${answer} n'est pas de ${correct.str.length} lettres. Entrer un mot de cette longueur.") 
-            make_proposal(retry, player, dict_machine, correct)
+        else if playerAnswers("next").isValidWord(currentPlayerAnswer)==false then 
+            println(s"${currentPlayerAnswer} n'est pas dans le dictionnaire du jeu. Entrer autre chose.")
+            make_proposal(retry, playerAnswers, machineAnswers, correct, hideMachine)
+        else if currentPlayerAnswer.length != correct.length then
+            println(s"${currentPlayerAnswer} n'est pas de ${correct.length} lettres. Entrer un mot de cette longueur.") 
+            make_proposal(retry, playerAnswers, machineAnswers, correct, hideMachine)
         else 
-            val proposal =  Proposal(player("dico").getSpecificWord(answer), correct)
-            proposal.isCorrect match
-                case true => println(s"Le mot est bien ${proposal}. VICTOIRE!!!")
+            val playerProposal =  Proposal(playerAnswers("next").getSpecificWord(currentPlayerAnswer), correct)
+            playerProposal.isCorrect match
+                case true => println(s"\n\nLe mot est bien ${playerProposal}. VICTOIRE!!!")
                     return()
                 case false => 
-                    val proposal2 = Proposal(dict_machine.getRandomWord(), correct)
-                    proposal2.isCorrect match {
-                        case true => println(s"La proposition ${proposal2} de l'ordinateur est correcte. DEFAITE...")
+                    val machineProposal = Proposal(machineAnswers("next").getRandomWord(), correct)
+                    machineProposal.isCorrect match {
+                        case true => println(s"\n\nLa proposition ${machineProposal} de l'ordinateur est correcte. DEFAITE...")
                             return()
                         case false =>
-                            val update2 = dict_machine.selectWords(toInclude=proposal2.goodLetters.toSet, toExclude=proposal2.badLetters.toSet)
+                            val machineUpdate = Map("next"->machineAnswers("next").selectWords(toInclude=machineProposal.goodLetters.toSet, toExclude=machineProposal.badLetters.toSet),
+                                                    "last"->Dict(fromList=machineAnswers("last").all_words:::List(machineProposal.answer)))
+                            val playerUpdate = Map("next"->playerAnswers("next"), 
+                                                   "last"->Dict(fromList=playerAnswers("last").all_words:::List(playerProposal.answer)))
                             clear()
-                            println(proposal2)
-                            val update = Map("dico"->player("dico"), "answer"->Dict(fromList=player("answer").all_words:::List(proposal.answer)))
-                            val histo = for (w<-update("answer").all_words) yield Proposal(w,correct)
-                            //update("answer").all_words.foldLeft(List[Word]()) ((xs:List[Object], w:Word)=> xs:::List(Proposal(w,correct)))
-                            println(s"Encore ${retry-1} essais.")
-                            histo foreach println
-                            make_proposal(retry-1, update, update2, correct) 
+                            println("Historique des propositions :\n")
+                            println("JOUEUR:   MACHINE:")
+                            for (i<- 0 to machineUpdate("last").size-1) yield println(s"${Proposal(playerUpdate("last").all_words(i),correct)}   ${Proposal(machineUpdate("last").all_words(i), correct, hideMachine)}")
+                            println("")
+                            make_proposal(retry-1, playerUpdate, machineUpdate, correct, hideMachine) 
                     }  
 
-    val main_dict = Dict("ods8.txt")
+    val mainDict = Dict("ods8.txt")
     @main
     def start_game() = 
         val retry = 7
-        val word_size = 7
-        val solution = main_dict.getRandomWord(word_size)
-        val human = Map("dico"->main_dict,"answer"->Dict())
-        val machine = main_dict.selectWords(word_size, solution.letterOrder.slice(0,1).toSet)
-        println(s"Mot de ${solution.str.length} lettres : ${solution.mask}")
-        make_proposal(retry, human, machine, solution)
+        val wordSize = 7
+        val hideMachine = true
+        val wordToFind = mainDict.getRandomWord(wordSize)
+        val playerAnswers = Map("next"->mainDict, "last"->Dict())
+        val machineAnswers = Map("next"->mainDict.selectWords(wordSize, wordToFind.letterOrder.slice(0,1).toSet), "last"->Dict())
+        println(s"Mot de ${wordToFind.length} lettres : ${wordToFind.mask}")
+        make_proposal(retry, playerAnswers, machineAnswers, wordToFind, hideMachine)
